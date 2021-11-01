71c1571a9fb22d43b2f8122f1f6a2389326f7b4f
Jared Behler <jediracer13@gmail.com>
2021-11-01 19:59:23 -0400

    update app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template, redirect, jsonify
-
+import pickle
+import numpy as np
 import os
 
 # sql_u = os.environ.get("sql_user", None)
@@ -9,8 +10,21 @@ import os
 
 app = Flask(__name__)
 
+# def recipeRater(test_recipe):
+    # testRecipe = np.array(test_recipe).reshape(1, 5)
+    # loaded_model = pickle.load(open("final_ML_model.pkl", "rb"))
+    # result = loaded_model.predict(testRecipe)
+    # return result[0]
+
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/testBeerRecipe')
+def testBeerRecipe():
+
+    # result = recipeRater(test_recipe)
+    # print result to a textbox
     return render_template('index.html')
 
 if __name__ == '__main__':
